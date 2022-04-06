3f33eea236ce0fed3b6d3d5a22609365515cdefb
murytarlah <dknight.jim@gmail.com>
2022-04-06 06:17:44 -0700

    program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,14 +6,13 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-	int total = 0;
+	int i, j, add = 0;
 
-	for (int i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
@@ -21,8 +20,8 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		total += atoi(argv[i]);
+		add += atoi(argv[i]);
 	}
-	printf("%d\n", total);
+	printf("%d\n", add);
 	return (0);
 }
