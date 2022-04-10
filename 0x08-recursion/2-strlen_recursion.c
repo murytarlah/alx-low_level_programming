8f483ca0985f22bc88fcfdea54779b65ec613be5
murytarlah <dknight.jim@gmail.com>
2022-04-10 11:13:17 -0700

    corrected return statement

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,8 +12,9 @@ int _strlen_recursion(char *s)
 	if (*s != '\0')
 	{
 		length += 1;
-		_strlen_recursion(s - 1);
+		_strlen_recursion(s + 1);
+		return (length);
 	}
 	else
-		return (length);
+		return (0);
 }
