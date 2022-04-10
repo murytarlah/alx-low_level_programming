4e9a0ab61d02927ea48f78266a78bf9a872d072d
murytarlah <dknight.jim@gmail.com>
2022-04-10 11:17:29 -0700

    used another approach

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,10 @@ int _strlen_recursion(char *s)
 {
 	int length = 0;
 
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		length += 1;
-		_strlen_recursion(s + 1);
-		return (length);
+		return (0);
 	}
 	else
-		return (0);
+		return (1 + _strlen_recursion(s + 1);
 }
