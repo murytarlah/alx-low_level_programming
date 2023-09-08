c4ee2c044a40383f58b67a5c7d598322e44a4ff8
murytarlah <murytarlah.dev@gmail.com>
2023-09-08 22:36:53 +0100

    0x01:task(10) - finished task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 100 with commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i,e;
+
+	i = 48;
+	e = 48;
+	
+	while (i < 58)
+	{
+		while (e < 58)
+		{
+			if (i != e && i < e)
+			{
+				putchar(i);
+				putchar(e);
+				if (i == 56 || e == 57)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			e++;
+		}
+		e = 48;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
