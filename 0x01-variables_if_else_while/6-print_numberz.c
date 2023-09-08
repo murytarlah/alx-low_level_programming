1d33331ce01eb201e0cdece138e4849d390d0b68
murytarlah <murytarlah.dev@gmail.com>
2023-09-08 22:13:25 +0100

    0x01:task(6) - fixed betty issue

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-    int i;
+	aint i;
 
-    for (i = 48; i < 58; i++)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
