761e398bef2c2e2c1284f7a324c4d5ecc9c2497a
murytarlah <murytarlah.dev@gmail.com>
2023-09-08 23:27:59 +0100

    0x01:task(11) - betty issue fixed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,29 +11,29 @@ int main(void)
 
 	i = 48;
 	e = 48;
-    j = 48;
-
+	j = 48;
+	
 	while (i < 58)
 	{
 		while (e < 58)
 		{
-            while (j < 58)
-            {
-                if (i != e && i != j && e != j && i < e && e < j)
-                {
-                    putchar(i);
-                    putchar(e);
-                    putchar(j);
-                    if (e == 56 && i == 55 && j == 57)
-                    {
-                        break;
-                    }
-                    putchar(',');
-                    putchar(' ');
-                }
-                j++;
-            }
-            j = 48;
+			while (j < 58)
+			{
+				if (i != e && i != j && e != j && i < e && e < j)
+				{
+					putchar(i);
+					putchar(e);
+					putchar(j);
+					if (e == 56 && i == 55 && j == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				j++;
+			}
+			j = 48;
 			e++;
 		}
 		e = 48;
