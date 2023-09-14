bf5643c3b7abe946d4ae89970031e1d478787248
murytarlah <murytarlah.dev@gmail.com>
2023-09-14 16:56:38 +0100

    0x02:task(3) - betty issues

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _islower: prints the 
+ * _islower: checks if the argument c is lower case
  * @c: character to check the case
  *
  * Return: 0 or 1
  */
 int _islower(int c)
 {
-	return(c >= 'a' && c <= 'z');
+	return (c >= 'a' && c <= 'z');
 }
