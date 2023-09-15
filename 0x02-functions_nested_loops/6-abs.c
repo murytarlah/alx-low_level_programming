641e1648572e05879b419bd3a75d5ecfaf0c3034
murytarlah <murytarlah.dev@gmail.com>
2023-09-15 13:04:55 +0100

    0x02:task(6) - finished task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer argument
+ *
+ * Return: absolute value of the number
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
