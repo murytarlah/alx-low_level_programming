73d8c81c6ec4ff261de7731284a8326f4ca80009
murytarlah <murytarlah.dev@gmail.com>
2023-09-15 13:38:24 +0100

    0x02:task(7) - betty issue and operation order fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,14 @@
  */
 
 int print_last_digit(int n)
-{	
+{
 	int l_digit;
+
 	if (n < 0)
-		l_digit = -1 * n % 10;
+		l_digit = -1 * (n % 10);
 	else
 		l_digit = n % 10;
-	
+
 	_putchar(l_digit + '0');
 	return (l_digit);
 }
