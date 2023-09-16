5874115a16b7d145332c4240a6f533b289d7dc0b
murytarlah <murytarlah.dev@gmail.com>
2023-09-16 12:00:31 +0100

    0x02:task(11) - refactored code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,9 @@
  */
 void print_to_98(int n)
 {
-	if (n == 98)
+	if (n < 98)
 	{
-		printf("%i", n);
-	}
-	else if (n < 98)
-	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%i, ", n);
 			n++;
@@ -22,11 +18,12 @@ void print_to_98(int n)
 	}
 	else
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%i, ", n);
 			n--;
 		}
 	}
+	printf("98");
 	_putchar('\n');
 }
