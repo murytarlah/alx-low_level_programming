2c78fa600d11917f4504f0b43560494273b1dd05
murytarlah <murytarlah.dev@gmail.com>
2023-09-16 11:20:33 +0100

    0x02:task(10) - finished task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers and return their result
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
