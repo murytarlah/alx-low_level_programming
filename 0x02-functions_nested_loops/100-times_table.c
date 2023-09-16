370b9579dd8f42730816a7d650915469cb20b670
murytarlah <murytarlah.dev@gmail.com>
2023-09-16 13:54:33 +0100

    0x02:task(100) - condition and betty issue

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,6 @@ void n_times_table(int n)
 			rep = a * b;
 			if (b == 0)
 				_putchar('0' + rep);
-			
 			else if (rep < 10)
 			{
 				_putchar(' ');
@@ -31,7 +30,7 @@ void n_times_table(int n)
 				_putchar('0' + rep % 10);
 			}
 			else
-			{	
+			{
 				_putchar('0' + rep / 100);
 				_putchar('0' + (rep - 100) / 10);
 				_putchar('0' + rep % 10);
@@ -55,7 +54,7 @@ void n_times_table(int n)
  */
 void print_times_table(int n)
 {
-	if (n > 0 && n	 <= 15)
+	if (n >= 0 && n	 <= 15)
 	{
 		n_times_table(n);
 	}
