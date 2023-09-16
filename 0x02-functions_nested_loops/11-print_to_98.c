bc0a016a7ba5b758785b0fc9723e3adc69891cd1
murytarlah <murytarlah.dev@gmail.com>
2023-09-16 11:51:59 +0100

    0x02:task(11) - completed task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints from n to 98
+ * @n: number to start from 
+ *
+ * Return: 0
+ */
+void print_to_98(int n){
+	if (n == 98)
+	{
+		printf("%i", n);
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%i ", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%i ", n);
+			n--;
+		}
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -12,5 +13,6 @@ int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
+void print_to_98(int n);
 
 #endif
